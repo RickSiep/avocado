96fcf76676dc6abd8ec1a71d5c88e58cbcb8ff26
RickSiep <rick-siepelinga@hotmail.com>
2022-04-14 16:27:00 +0200

    Added caching

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\View\View;
 
 class ApiController extends Controller
 {
-    public function welcome() {
-        $collection = Http::get('https://www.swapi.tech/api/films')->collect();
+    public function welcome(): View {
+        // Check in the cache if the data is still there, so that on refresh load times are faster
+        $collection = Cache::get('movies');
+        if (is_null($collection)) $collection = Http::get('https://www.swapi.tech/api/films')->collect();
+        Cache::put('movies', $collection, now()->addMinutes(20));
+
         return view('welcome', ['collection' => $collection['result']]);
     }
 }
